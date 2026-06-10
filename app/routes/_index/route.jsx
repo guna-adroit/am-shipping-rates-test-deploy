fad74f5855bdade639f8db00fc8973aa7c1b2562
Gunasekaran <gunasekaran.matheshwaran@adroitminds.com>
2026-06-10 14:54:19 +0530

    App URL redirect fix

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css";
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
 
-  if (url.searchParams.get("shop")) {
+  if (url.searchParams.get("shop") || url.searchParams.get("host") || url.searchParams.get("hmac")) {
     throw redirect(`/app?${url.searchParams.toString()}`);
   }
 
